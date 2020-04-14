e309fa6acbbbd86ba84e04c135e5a3161ec47836
smith123417458 <a0952987429@yahoo.com.tw>
2020-04-14 20:36:32 +0800

    'icon'

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,103 +1,133 @@
 <template>
+
+<div>
+
   <nav
     class="navbar navbar-expand-xl p-0 fixed-top navDefault"
-   
   >
- 
-     <div class="collapse navbar-collapse ml-xl-6 order-2 order-xl-1" >
-      
-    </div>
-  
-    <ul class="navbar-nav mr-xl-6 order-1 order-xl-2">
+    <a class="navbar-brand" href="#">
+      <img
+        src="../assets/img/r02.png"
+        width="30"
+        height="30"
+        class="d-inline-block align-top ml-4"
+      />
      
-       <li class="nav-item">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-       原生的
-    </button>  
-       </li> 
-          <li class="nav-item">
+    </a>
+    <div class="collapse navbar-collapse ml-xl-6 order-2 order-xl-1">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link p-3 px-4" href="#" @click.prevent="goNewArrival">
+            <!-- <span class="hvr-underline-from-left">首頁</span> -->
+            <img src="../assets/img/homepage.png" />
+          </a>
         </li>
-    </ul>
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" >
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+        <!-- 瀏覽器下車款選擇 -->
+        <li class="nav-item d-none d-xl-block">
+          <a
+            id="brand"
+            class="nav-link p-3 px-4"
+          >
+            <span class="align-middle">車款選擇</span>
+          </a>
+        </li>
+      </ul>
     </div>
-  </div>
-</div>  
+    <!-- 會員登入 -->
+    <ul class="navbar-nav mr-xl-6 order-1 order-xl-2">
+      <li class="nav-item">
+        <router-link class="d-none d-xl-block py-3 px-4" to="/signin">
+        
+          <img src="../assets/img/log-in.png" />
+        </router-link>
+      </li>
+      <!-- 購物車按鈕 -->
+      <li class="nav-item">
+        <router-link class="position-relative d-block py-3 px-4" to="/cart">
+          <i class="fal fa-shopping-cart"></i>
+          <p
+            class="position-absolute text-center text-white bg-danger rounded-circle"
+            style="top:10%; right:25%; font-size: 12px; min-width: 17px"
+            v-if="this.$store.state.carts.carts.length"
+          >{{ this.$store.state.carts.carts.length }}</p>
+        </router-link>
+      </li>
 
+      <li class="nav-item">
+        <div class="btn-group favorite d-inline-block">
+          <button type="button" class="btn favorite-btn" data-toggle="dropdown">
+            <img src="../assets/img/heart.png" />
+            <span class="badge badge-pill badge-success">{{ favoritesLength }}</span>
+          </button>
+          <div class="dropdown-menu dropdown-menu-right">
+            <div class="pt-2 px-3">
+              <h5 class="text-center">我的最愛</h5>
+              <table class="table mb-2" style="min-width:200px">
+                <tbody>
+                  <tr class="favorite-list" v-for="favorite in favorites" :key="favorite.id">
+                    <td class="py-2" width="30">
+                      <a
+                        href="#"
+                        class="text-danger favorite-list-delbtn"
+                        @click.prevent="removeFavorite(favorite, false)"
+                      >
+                        <i class="fas fa-times"></i>
+                      </a>
+                    </td>
+                    <td class="py-2">
+                      <router-link
+                        :to="`/productslist/${favorite.id}`"
+                        class="d-block"
+                      >{{ favorite.title }}</router-link>
+                    </td>
+                  </tr>
+                 
+                </tbody>
+              </table>
 
+             
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
 
-
- 
+  
   </nav>
+
+
+
+
+
+  </div>
+
+
+
+
+
+
 
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-  data() {
-    return {  
-    
-   
-    };
-  },
-  computed:{
-   
-  ...mapGetters('favoriteModules', ['favorites', 'favoritesLength']),
-
-  }
-   ,
-  
-  
-  methods: {
-   
-   
-     
-
-   
  
-   
-
-    removeFavorite(favorite, delall) {
-      this.$store.dispatch('favoriteModules/removeFavorite', { favoriteItem: favorite, delall });
+  computed: {
+    getcar() {
+      return this.$store.state.carts.carts.length;
     },
-    ...mapActions('favoriteModules', ['getFavorite']),
-
-
-
-
-
-
-
-
-
-
-
-
-
+    ...mapGetters("favoriteModules", ["favorites", "favoritesLength"])
   },
+
+  
+ 
   created() {
-   
-
-    this.$store.dispatch('favoriteModules/getFavorite');
-
-
-  },  
+    window.addEventListener("scroll", this.windowScroll, false);
+    this.$store.dispatch("getCarts");
+    this.$store.dispatch("favoriteModules/getFavorite");
+  }
 };
 </script>
 
@@ -107,37 +137,5 @@ export default {
 
 
 
-// ================================================================================
-
-
- .favorite {
-  float: right;
-  margin-left: 0.25rem;
-}
-
- .favorite-btn {
-  float: right;
-  position: relative;
-  background-color: transparent;
-  text-align: right;
-  font-size: inherit;
-  .badge {
-    position: absolute;
-    top: auto;
-    bottom: 2px;
-    right: -1px;
-  }
-}
-.favorite-list {
-  &:hover, &:active {
-    background-color: red;
-    text-decoration: underline;
-  }
-  .favorite-list-delbtn:hover, .favorite-list-delbtn:active {
-    i {
-      font-size: 1.25rem;
-    }
-  }
-}
 
 </style>
